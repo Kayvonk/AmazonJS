acfa0544a15e552bd83236849559b9735877105c
Kayvon Kazemini <kayvonk@gmail.com>
2021-08-12 12:08:31 -0600

    Deploying to firebase

---
+++ public/firebase.js
@@ -0,0 +1,12 @@
+  var firebaseConfig = {
+    apiKey: "AIzaSyBaCP2cQJqcM4Qym4nSL-sNNKGNXi7V3zI",
+    authDomain: "kshopjs.firebaseapp.com",
+    projectId: "kshopjs",
+    storageBucket: "kshopjs.appspot.com",
+    messagingSenderId: "711500363864",
+    appId: "1:711500363864:web:297c9fb5526a7e7861f22d"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var db = firebase.firestore();
